94f8ed69700257f2df23ffe52cd32b72bcbeb07f
steven-fo <stevenfo0711@gmail.com>
2023-11-15 15:30:34 +0700

    finish tutorial 8

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 import datetime
-from django.http import HttpResponseNotFound, HttpResponseRedirect
+import json
+from django.http import HttpResponseNotFound, HttpResponseRedirect, JsonResponse
 from main.forms import ProductForm
 from django.urls import reverse
 
@@ -126,3 +127,20 @@ def add_product_ajax(request):
         return HttpResponse(b"CREATED", status=201)
 
     return HttpResponseNotFound()
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
